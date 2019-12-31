1e63f5a836fb3bf73aa206a07910839e58240308
Nikolaus Köstinger <nikolaus.koestinger@sbg.at>
2019-12-31 13:45:31 +0100

    created navigation bar, side navigation on mobile, quick options menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  title = 'AccountX';
+  currentYear;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.currentYear = new Date().getFullYear().toString();
+  }
 }
