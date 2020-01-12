eaedefa01518928e88b3239bdfdad185fae7058f
DiOps <nikolaus.koestinger@edu.fh-joanneum.at>
2020-01-12 18:20:21 +0100

    minor code design changes

--- src/app/vat/vat.component.ts
+++ src/app/vat/vat.component.ts
@@ -1,7 +1,7 @@
 import { VatService } from './../service/vat.service';
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import * as M from 'materialize-css';
-import {UserService} from '../service/user.service';
+import { UserService } from '../service/user.service';
 
 @Component({
   selector: 'app-vat',
@@ -10,22 +10,22 @@ import {UserService} from '../service/user.service';
 })
 export class VatComponent implements OnInit, AfterViewInit {
 
-  vatReport = ["", "", "", "", "", "", "", "", "", "", "", ""]; // initialize array
-  //months = ["Jan", "Feb", "Mar", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"];
-  thisYear = new Date().getFullYear().toString();
+  vatReport = ['', '', '', '', '', '', '', '', '', '', '', '']; // initialize array
+  // months = ["Jan", "Feb", "Mar", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"];
+  currentYear = new Date().getFullYear().toString();
   months = [
-      ["Jan", this.thisYear + "-02-01", this.thisYear + "-01-01"],
-      ["Feb", this.thisYear + "-03-01", this.thisYear + "-02-01"],
-      ["Mar", this.thisYear + "-04-01", this.thisYear + "-03-01"],
-      ["Apr", this.thisYear + "-05-01", this.thisYear + "-04-01"],
-      ["Mai", this.thisYear + "-06-01", this.thisYear + "-05-01"],
-      ["Jun", this.thisYear + "-07-01", this.thisYear + "-06-01"],
-      ["Jul", this.thisYear + "-08-01", this.thisYear + "-07-01"],
-      ["Aug", this.thisYear + "-09-01", this.thisYear + "-08-01"],
-      ["Sep", this.thisYear + "-10-01", this.thisYear + "-09-01"],
-      ["Okt", this.thisYear + "-11-01", this.thisYear + "-10-01"],
-      ["Nov", this.thisYear + "-12-01", this.thisYear + "-11-01"],
-      ["Dez", this.thisYear + "-12-31", this.thisYear + "-12-01"]
+    ['Jan', this.currentYear + '-02-01', this.currentYear + '-01-01'],
+    ['Feb', this.currentYear + '-03-01', this.currentYear + '-02-01'],
+    ['Mar', this.currentYear + '-04-01', this.currentYear + '-03-01'],
+    ['Apr', this.currentYear + '-05-01', this.currentYear + '-04-01'],
+    ['Mai', this.currentYear + '-06-01', this.currentYear + '-05-01'],
+    ['Jun', this.currentYear + '-07-01', this.currentYear + '-06-01'],
+    ['Jul', this.currentYear + '-08-01', this.currentYear + '-07-01'],
+    ['Aug', this.currentYear + '-09-01', this.currentYear + '-08-01'],
+    ['Sep', this.currentYear + '-10-01', this.currentYear + '-09-01'],
+    ['Okt', this.currentYear + '-11-01', this.currentYear + '-10-01'],
+    ['Nov', this.currentYear + '-12-01', this.currentYear + '-11-01'],
+    ['Dez', this.currentYear + '-12-31', this.currentYear + '-12-01']
   ];
   companyId = this.userService.getCompanyToken();
 
@@ -50,14 +50,18 @@ export class VatComponent implements OnInit, AfterViewInit {
   }
 
   getData() {
-      this.months.forEach((element, index) => {
-          this.vatService.getVatReport(this.companyId, element[1], element[2]).subscribe(
-              (response) => {
-                  response[0]['month'] = element[0];
-                  this.vatReport[index] = response[0];
-              }
-          );
-      });
+    this.months.forEach((element, index) => {
+      this.vatService.getVatReport(this.companyId, element[1], element[2]).subscribe(
+        (response) => {
+          // was genau passiert hier?
+          response[0].month = element[0]; // changed from response[0]['month'] = element[0];
+          console.log(response[0]);
+          this.vatReport[index] = response[0];
+          console.log(this.vatReport);
+
+        }
+      );
+    });
   }
 
 }
