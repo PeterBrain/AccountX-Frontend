763164c21be99d46486976c8c216b15f81d7128c
PeterBrain <peter.loecker@live.at>
2020-01-22 19:49:52 +0100

    button titles; export for sales

--- src/app/sales/sales.component.ts
+++ src/app/sales/sales.component.ts
@@ -2,6 +2,7 @@ import { ActivatedRoute } from '@angular/router';
 import { SaleService } from './../service/sale.service';
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import * as M from 'materialize-css';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-sales',
@@ -42,6 +43,19 @@ export class SalesComponent implements OnInit, AfterViewInit {
 
     const actionBtn = document.querySelectorAll('.fixed-action-btn');
     M.FloatingActionButton.init(actionBtn);
+  }
+
+  exportexcel(tableId, fileName) {
+    /* table id is passed over here */
+    const element = document.getElementById(tableId);
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, fileName);
   }
 
 }
