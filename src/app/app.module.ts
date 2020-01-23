51483f9cc7435679237bbe8b7da47c02b323ec0e
PeterBrain <peter.loecker@live.at>
2020-01-23 01:52:59 +0100

    error toast with interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,29 @@
-import { MediainputComponent } from './mediainput/mediainput.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { SalesComponent } from './sales/sales.component';
-import { PurchasesComponent } from './purchases/purchases.component';
-import { VatComponent } from './vat/vat.component';
-import { PurchasesFormComponent } from './purchases-form/purchases-form.component';
-import { SalesFormComponent } from './sales-form/sales-form.component';
-import { HttpClientModule } from '@angular/common/http';
-import { LoginComponent } from './login/login.component';
-import { LogoutComponent } from './logout/logout.component';
-import { JwtModule } from '@auth0/angular-jwt';
+import {MediainputComponent} from './mediainput/mediainput.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {SalesComponent} from './sales/sales.component';
+import {PurchasesComponent} from './purchases/purchases.component';
+import {VatComponent} from './vat/vat.component';
+import {PurchasesFormComponent} from './purchases-form/purchases-form.component';
+import {SalesFormComponent} from './sales-form/sales-form.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {LoginComponent} from './login/login.component';
+import {LogoutComponent} from './logout/logout.component';
+import {JwtModule} from '@auth0/angular-jwt';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { CompanySelectComponent } from './company-select/company-select.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
-import { CompanyFormComponent } from './company-form/company-form.component';
-import { UserFormComponent } from './user-form/user-form.component';
-import { FileUploadModule } from 'ng2-file-upload';
-import { FilterPipe } from './admin-dashboard/filter.pipe';
+import {CompanySelectComponent} from './company-select/company-select.component';
+import {RegistrationComponent} from './registration/registration.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {AdminDashboardComponent} from './admin-dashboard/admin-dashboard.component';
+import {CompanyFormComponent} from './company-form/company-form.component';
+import {UserFormComponent} from './user-form/user-form.component';
+import {FileUploadModule} from 'ng2-file-upload';
+import {FilterPipe} from './admin-dashboard/filter.pipe';
+import {InterceptorService} from './service/interceptor.service';
+
 export function tokenGetter() {
     return localStorage.getItem('access_token');
 }
@@ -46,22 +47,28 @@ export function tokenGetter() {
         MediainputComponent,
         FilterPipe
     ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NoopAnimationsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    FileUploadModule,
-    JwtModule.forRoot({
-      config: {
-        tokenGetter: tokenGetter,
-        whitelistedDomains: ['localhost:4200']
-      }
-    }),
-    FormsModule
-  ],
-    providers: [],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NoopAnimationsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        FileUploadModule,
+        JwtModule.forRoot({
+            config: {
+                tokenGetter: tokenGetter,
+                whitelistedDomains: ['localhost:4200']
+            }
+        }),
+        FormsModule
+    ],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: InterceptorService,
+            multi: true
+        }
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ src/app/service/interceptor.service.ts
@@ -0,0 +1,38 @@
+import {Injectable} from '@angular/core';
+import {
+    HttpInterceptor, HttpRequest,
+    HttpHandler, HttpEvent, HttpErrorResponse
+} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+import * as M from 'materialize-css';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class InterceptorService implements HttpInterceptor {
+    constructor() {
+    }
+
+    handleError(error: HttpErrorResponse) {
+        // console.log('Oops... that shouldn\'t have happened!');
+
+        let errorMessage = '';
+        if (error.error instanceof ErrorEvent) {
+            errorMessage = `Error: ${error.error.message}`; // client-side error
+        } else {
+            errorMessage = `Error Code: ${error.status}<br>Message: ${error.message}`; // server-side error
+        }
+
+        M.toast({
+            html: errorMessage,
+            displayLength: 10000
+        });
+
+        return throwError(error);
+    }
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(req).pipe(catchError(this.handleError));
+    };
+}
