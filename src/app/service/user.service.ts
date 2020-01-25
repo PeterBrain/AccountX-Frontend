40cec0f961c4c6124264b523eecd400a90f0c60a
Kevin <kazianschuetz.kevin@gmail.com>
2020-01-25 18:29:19 +0100

    Translation error

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -43,7 +43,7 @@ export class UserService {
 
                 this.router.navigate(['firmen']);
             }, () => {
-                this.showToastRed('Wrong username or password');
+                this.showToastRed('Benutzername oder Passwort falsch');
             });
     }
 
